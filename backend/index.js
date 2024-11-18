41d676cb2db22410614a23994220b5296c0120de
Archit301 <tambiarchit@gmail.com>
2024-11-19 02:32:43 +0530

    modified:   backend/index.js 	modified:   package.json

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import userRoutes from "./routes/user_route.js"
 import articleRoutes from "./routes/article_route.js"
 import commentRoutes from "./routes/comment_route.js"
 import questionRoutes from "./routes/question_route.js"
+import path from "path"
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -22,8 +23,14 @@ app.use('/api/articles', articleRoutes);
 app.use('/api/comments', commentRoutes);
 app.use('/api/questions', questionRoutes);
 
-
+const __dirname = path.resolve();
  const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+})
